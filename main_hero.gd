ffaa39427846842e70eee0c97b339a3c5d03c95b
unknown <zgergo51@gmail.com>
2026-09-23 13:52:46 +0200

    09.23 Walking anim

--- main_hero.gd
+++ main_hero.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 const NORMAL_SPEED = 300.0
 const SPRINT_SPEED = 400.0
+var last_direcion = ""
 
 
 func _physics_process(delta: float) -> void:
@@ -10,14 +11,79 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		velocity = direction * SPEED
+		if direction.y > 0:
+			last_direcion = "up"
+		elif direction.y < 0:
+			last_direcion = "down"
+		elif direction.x > 0:
+			last_direcion = "right"
+		else:
+			last_direcion = "left"
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	
-	if direction.y < 0:
-		$hero_back_look.show()
-		$hero_front_look.hide()
-	elif direction.y > 0:
+	if direction.y != 0 or direction.x != 0:
 		$hero_back_look.hide()
-		$hero_front_look.show()
+		$hero_front_look.hide()
+		$hero_left_look.hide()
+		$hero_right_look.hide()
+		if direction.y < 0:
+			$hero_back_walk.show()
+			$hero_back_walk.play("default")
+			$hero_front_walk.hide()
+			$hero_front_walk.stop()
+			$hero_left_walk.hide()
+			$hero_left_walk.stop()
+			$hero_right_walk.hide()
+			$hero_right_walk.stop()
+		elif direction.y > 0:
+			$hero_front_walk.show()
+			$hero_front_walk.play("default")
+			$hero_back_walk.hide()
+			$hero_back_walk.stop()
+			$hero_left_walk.hide()
+			$hero_left_walk.stop()
+			$hero_right_walk.hide()
+			$hero_right_walk.stop()
+		elif direction.x > 0:
+			$hero_right_walk.show()
+			$hero_right_walk.play("default")
+			$hero_back_walk.hide()
+			$hero_back_walk.stop()
+			$hero_front_walk.hide()
+			$hero_front_walk.stop()
+			$hero_left_walk.hide()
+			$hero_left_walk.stop()
+		elif direction.x < 0:
+			$hero_left_walk.show()
+			$hero_left_walk.play("default")
+			$hero_back_walk.hide()
+			$hero_back_walk.stop()
+			$hero_front_walk.hide()
+			$hero_front_walk.stop()
+			$hero_right_walk.hide()
+			$hero_right_walk.stop()
+	if direction.y == 0 and direction.x == 0:
+		$hero_back_walk.hide()
+		$hero_back_walk.stop()
+		$hero_front_walk.hide()
+		$hero_front_walk.stop()
+		$hero_left_walk.hide()
+		$hero_left_walk.stop()
+		$hero_right_walk.hide()
+		$hero_right_walk.stop()
+		if last_direcion == "up":
+			$hero_front_look.show()
+		elif last_direcion == "down":
+			$hero_back_look.show()
+		elif last_direcion == "right":
+			$hero_right_look.show()
+		elif last_direcion == "left":
+			$hero_left_look.show()
+	
+
+
+
+
 
 	move_and_slide()
